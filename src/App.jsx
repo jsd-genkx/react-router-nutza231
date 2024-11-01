3f1bc5df118bd6b5cefdbda606393e23e2b9065c
nutza <www.nutzazaza@gmail.com>
2024-11-01 14:58:41 +0700

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import "./App.css";
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+import About from "./pages/About"
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-50">
+      Hello world!
+    </h1>
+  )
+}
